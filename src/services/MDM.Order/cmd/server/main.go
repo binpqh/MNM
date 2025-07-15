66e58653b366a7cf1a31d9d9bef7d132e72740dc
Nho Ánh <219388631+nanh961@users.noreply.github.com>
2025-07-15 21:24:33 +0700

    Split routers from implementation at main.go

--- src/services/MDM.Order/cmd/server/main.go
+++ src/services/MDM.Order/cmd/server/main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
+	"github.com/binpqh/MDM.Order/internal/routers"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	r := routers.NewDefaultRouter()
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
