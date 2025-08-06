5317029582c4512bc0c9c03fb0a6422fba3b96d8
Bin Phạm <binpro0203@gmail.com>
2025-08-06 10:44:27 +0700

    Clean up whitespace in models.py

--- src/services/MDM.Order/app/models.py
+++ src/services/MDM.Order/app/models.py
@@ -4,11 +4,8 @@ from typing import Optional
 from bson import ObjectId
 from pydantic import BaseModel, Field
 from enum import Enum
-
-
 class PyObjectId(ObjectId):
     """Custom Pydantic-compatible ObjectId Type"""
-
     @classmethod
     def __get_validators__(cls):
         yield cls.validate
@@ -18,8 +15,6 @@ class PyObjectId(ObjectId):
         if not ObjectId.is_valid(v):
             raise ValueError('Invalid ObjectId')
         return ObjectId(v)
-
-
 class Entity(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     created_on: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
@@ -29,7 +24,6 @@ class Entity(BaseModel):
     updated_by : Optional[PyObjectId] = None
     deleted_by : Optional[PyObjectId] = None
     is_deleted : bool = False
-
     class Config:
         allow_population_by_field_name = True
         json_encoders = {ObjectId: str}
